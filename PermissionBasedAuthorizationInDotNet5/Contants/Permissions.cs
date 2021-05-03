378db4c7c5bc2658ccca7e5cbfe54ae468deabd9
Abdelmoniem Selim <Abdelmoniem_Selim@outlook.com>
2021-05-04 00:03:55 +0200

    Initial Commit

---
+++ PermissionBasedAuthorizationInDotNet5/Contants/Permissions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionBasedAuthorizationInDotNet5.Contants
+{
+    public static class Permissions
+    {
+        public static List<string> GeneratePermissionsList(string module)
+        {
+            return new List<string>()
+            {
+                $"Permission.{module}.View",
+                $"Permission.{module}.Create",
+                $"Permission.{module}.Edit",
+                $"Permission.{module}.Delete",
+            };
+        }
+    }
+}
---
+++ PermissionBasedAuthorizationInDotNet5/Contants/Roles.cs
@@ -0,0 +1,15 @@
+﻿namespace PermissionBasedAuthorizationInDotNet5.Contants
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Bassic
+    }
+
+    public enum Type
+    {
+        Permission,
+        Products
+    }
+}
